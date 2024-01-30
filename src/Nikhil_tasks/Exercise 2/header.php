4809b6675b42906c838389812bf363f7dbe80d70
Nikhil <nikhil23001@student.hamk.fi>
2024-01-30 18:11:05 +0200

    Added Exercise one to Abstract Hub Website

--- src/Nikhil_tasks/Exercise 2/header.php
+++ src/Nikhil_tasks/Exercise 2/header.php
@@ -19,6 +19,10 @@
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ml-auto">
                         <li class="nav-item">
+                            <a class="nav-link" href="../Exercise1.php">Exercise 1</a>
+                            <!-- use ../ to go one level behind the source folder-->
+                        </li>
+                        <li class="nav-item">
                             <a class="nav-link" href="Home.php">Home</a>
                         </li>
                         <li class="nav-item">
